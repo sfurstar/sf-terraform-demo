aafe0aae5b01a76a9c85880dc07a25cbb4ea708a
sfurstar <sfurstar@gmail.com>
2022-04-21 14:41:02 -0500

    update variables

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "snowflake_database" "db" {
 resource "snowflake_warehouse" "warehouse" {
   provider       = snowflake.sys_admin
   name           = "TF_DEMO"
-  warehouse_size = "large"
+  warehouse_size = "small"
 
   auto_suspend = 60
 }
